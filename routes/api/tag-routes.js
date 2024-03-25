7265cfec1f843b25c8a300c39f5deece5f16b0b0
ThatGiodude <giovanniFStrangio@gmail.com>
2024-03-25 19:48:04 -0400

    finished tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,6 +63,20 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 
 router.delete('/:id', (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!tagData){
+      res.status(404).json({ message: 'No tag found with this id!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  }catch(err) {
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
