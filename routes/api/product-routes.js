8f282926a00da3dba43467056d2a9009ba039b9b
ThatGiodude <giovanniFStrangio@gmail.com>
2024-03-25 18:13:37 -0400

    finished product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,55 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model:Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name'],
+          through: ProductTag,
+          as: 'tags'
+        }
+      ]
+    });
+    res.status(200).json(productData);
+  }catch(err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name'],
+          through: ProductTag,
+          as: 'tags'
+        }
+      ]
+    })
+    if(!productData) {
+      res.status(404).json({message: 'No product found with this id!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -93,6 +135,20 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  try{
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if(!productData) {
+      res.status(404).json({message: 'No product found with this id!'});
+      return;
+    }
+    res.status(200).json(productData);
+  }catch (err){
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
